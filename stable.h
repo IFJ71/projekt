a6c03c3e72c68993921b0aa5759ac317c1032509
filija <filija.jakub@gmail.com>
2014-12-03 13:26:58 +0100

    create struct

--- stable.h
+++ stable.h
@@ -1,24 +1,38 @@
 // definice jednotlivych typu
 #define TYPE_INT     1
-
-
+#define FCE 201
+#define IDENT 202
+#include "str.h"
 typedef struct
 {
-  int varType;  // typ dane promenne, v nasem pripade vzdy int
-  int varValue; // pro ruzne typy nutnost resit pomoci unie
+  int varType;  // typ dane promenne
+  union varValue hodnota; // pro ruzne typy nutnost resit pomoci unie
 } tData;
+
+union varValue
+{	
+	int integer;
+	bool boolean;
+	double real;
+	string str;
+	
+}varValue;
   
 typedef struct tableItem
 {
+  int typ;
   string key;                  // klic, podle ktereho se bude vyhledavat = nazev identifikatoru
   tData data;                  // data, ktera jsou ke klici pridruzena
   struct tableItem *nextItem;  // ukazatel na dalsi prvek tabulky symbolu
+  
 } tTableItem;
 
 typedef struct
 {
   struct tableItem *first;
 } tSymbolTable;
+
+
 
 void tableInit(tSymbolTable *T);
 int tableInsert(tSymbolTable *T, string *key, int varType);
