7e7d2d82c537e135c8f0c7fd2ea191887b8185dd
Warcaller <warcallerthefirst@gmail.com>
2014-12-10 12:02:13 +0100

    Pridani tokenu reprezentujici identifikator funkce

--- parser.h
+++ parser.h
@@ -1,14 +1,34 @@
-#include "scanner.h"
-#include "stable.h"
+#ifndef PARSER_H
+#define PARSER_H
 
-#define SYNTAX_OK     4
-#define SYNTAX_ERROR -2
-#define SEM_ERROR    -3
+#include "ial.h"
+#include "stable.h"
+#include "errors.h"
+#include "scanner.h"
+
+#define SYNTAX_OK     0
 #define LEX_ERROR     LEXIKALNICHYBA
 
 // hlavicka pro syntakticky analyzator
-int parse(tSymbolTable *ST, tListOfInstr *instrList);
+int parse(tSymbolTable *ST/*, tListOfInstr *instrList*/);
 
 int start();
-int declaration();
-int type(tTableItem *item);
+int prom_dekl();
+int typ(/* ! */);
+int prom_dekl2();
+int funkce();
+int sez_param(/* ! */);
+int sez_param2(/* ! */);
+int telofunkce(/* ! */);
+int telo();
+int skup_prikaz();
+int prikaz();
+int skup_prikaz2();
+int prirazeni();
+int po_prirazeni(/* ! */);
+int vol_param(/* ! */);
+int hodnota();
+int vol_param2();
+
+
+#endif
