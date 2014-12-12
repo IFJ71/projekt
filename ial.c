02354e71bf8befed42d911d144738b7a3155dd44
xstast24 <stastny.F@seznam.cz>
2014-12-12 23:30:44 +0100

    tabulka funkci - Filip S.
    tabulka funkci s heldanim a vkladanim, zatim bez uvolneni

--- ial.c
+++ ial.c
@@ -1,5 +1,6 @@
 #include "ial.h"
 
+//v v v hledani podretezce KNUTH/MORIS/PRATH algoritmus v v v
 int kmp_search(string S, string W)
 {
 	int m = 0;
@@ -65,6 +66,8 @@ void kmp_table(string W, int *T)
 	}
 }
 
+
+//v v v MERGE SORT v v v
 string merge_sort(string m)
 {
 	int length = strGetLength(&m);
@@ -121,3 +124,234 @@ string merge(string left, string right)
 	}
 	return result;
 }
+
+
+//v v v TABULKA SYMBOLU a jeji funkce v v v
+
+void tableInit(tSymbolTable *T)
+//funkce inicializuje tabulku symbolu
+{
+	T->first = NULL;
+}
+
+tTableItem *tableSearch(tSymbolTable *T, string *key)
+// pokud se dana polozka s klicem |key| v tabulce symbolu nachazi,
+// funkce vrati ukazatel na tuto polozku, jinak NULL
+{
+	tTableItem *ptr = T->first;
+	int found = 0;
+  
+	while ((!found) && (ptr != NULL))	//dokud nenajde, nebo nedojde na konec stromu
+	{
+		if(strCmpString(key, &(ptr->key)) == 0)
+		{
+			found = 1;
+		}
+		else if(strCmpString(key, &(ptr->key)) < 0)	//hledany klic je mensi, prohledej levy podstrom
+			ptr = ptr->leftptr;
+		else if(strCmpString(key, &(ptr->key)) > 0)	//hledany klic je vetsi, prohledej pravy podstrom
+			ptr = ptr->rightptr;
+	} 
+	return ptr;
+}
+
+tTableItem *tableSearchVar(tSymbolTable *T, string *key)
+// pokud se promenna s nazvem |key| v tabulce symbolu nachazi,
+// funkce vrati ukazatel na tuto polozku, jinak NULL
+{
+	tTableItem *ptr = T->first;
+	int found = 0;
+  
+	while ((!found) && (ptr != NULL))	//dokud nenajde, nebo nedojde na konec stromu
+	{
+		if(strCmpString(key, &(ptr->key)) == 0)
+		{
+			found = 1;
+			if(ptr->typ != VAR) ptr == NULL;		//pokud neni vysledek promenna (VAR), vrati NULL
+		}
+		else if(strCmpString(key, &(ptr->key)) < 0)	//hledany klic je mensi, prohledej levy podstrom
+			ptr = ptr->leftptr;
+		else if(strCmpString(key, &(ptr->key)) > 0)	//hledany klic je vetsi, prohledej pravy podstrom
+			ptr = ptr->rightptr;
+	} 
+	return ptr;	
+}
+
+tTableItem *tableSearchFun(tSymbolTable *T, string *key)
+// pokud se funkce s nazvem |key| v tabulce symbolu nachazi,
+// funkce vrati ukazatel na tuto polozku, jinak NULL
+{
+	tTableItem *ptr = T->first;
+	int found = 0;
+  
+	while ((!found) && (ptr != NULL))	//dokud nenajde, nebo nedojde na konec stromu
+	{
+		if(strCmpString(key, &(ptr->key)) == 0)
+		{
+			found = 1;
+			if(ptr->typ != FUN) ptr == NULL;		//pokud neni vysledek funkce (FUN), vrati NULL
+		}
+		else if(strCmpString(key, &(ptr->key)) < 0)	//hledany klic je mensi, prohledej levy podstrom
+			ptr = ptr->leftptr;
+		else if(strCmpString(key, &(ptr->key)) > 0)	//hledany klic je vetsi, prohledej pravy podstrom
+			ptr = ptr->rightptr;
+	} 
+	return ptr;
+}
+
+
+tTableItem *tableInsertVar(tSymbolTable *T, string *key)
+// funkce vlozi do tabulky symbolu NOVOU promennou, pokud nazev (key) jiz existuje nebo nastala chyba alokace, vraci NULL
+{
+	tTableItem *ptr = T->first; 
+	tTableItem *new = NULL;
+	int found = 0;
+	int end = 0;
+	
+	if(ptr == NULL)		//tabulka prazdna, vlozime promennou
+	{
+		if((new = (tTableItem*) malloc(sizeof(tTableItem))) != NULL)		//kontrola mallocu
+		{
+			//vlozeni typu (VAR) a nazvu (key)
+			new->typ = VAR;
+			strInit(&(new->key));
+			strCopyString(&(new->key), key);
+			T->first = new;			
+		}
+	}
+	else	//zjistime, zda se v tabulce polozka s danym klicem jiz nenachazi a zapiseme ji
+	{
+		while ((!found) && (ptr != NULL) && (end == 0))	//dokud nenajde, nebo nedojde na konec stromu
+		{
+			if(strCmpString(key, &(ptr->key)) == 0)
+			{
+				found = 1;
+				ptr = new = NULL;
+			}
+			else if(strCmpString(key, &(ptr->key)) < 0)	//hledany klic je mensi, prohledej levy podstrom
+				if(ptr->leftptr != NULL) ptr = ptr->leftptr;
+				else end = 1;	//koncime v levem podstromu
+			else if(strCmpString(key, &(ptr->key)) > 0)	//hledany klic je vetsi, prohledej pravy podstrom
+				if(ptr->rightptr != NULL) ptr = ptr->rightptr;
+				else end = 2;	//koncime v pravem podstromu
+		} 
+		if(found==0 && end != 0)	//prvek nebyl nalezen, muzeme proto vlozit novy
+		{
+			if((new = (tTableItem*) malloc(sizeof(tTableItem))) != NULL)		//kontrola mallocu
+			{
+			//vlozeni typu (VAR) a nazvu (key)
+			new->typ = VAR;
+			strInit(&(new->key));
+			strCopyString(&(new->key), key);		
+			if(end == 1) ptr->leftptr = new;		//prvek vlozen do leveho podstromu
+			if(end == 2) ptr->rightptr = new;		//prvek vlozen do praveho podstromu
+			}
+		}
+	}
+	return new;
+}
+
+
+tTableItem *tableInsertFun(tSymbolTable *T, string *key)
+// funkce vlozi do tabulky symbolu NOVOU funkci, pokud nazev (key) jiz existuje nebo nastala chyba alokace, vraci NULL
+//zaroven inicializuje lokalni tabulku symoblu T->data->localTable
+{
+	tTableItem *ptr = T->first; 
+	tTableItem *new = NULL;
+	int found = 0;
+	int end = 0;
+	
+	if(ptr == NULL)		//tabulka prazdna, vlozime funkci
+	{
+		if((new = (tTableItem*) malloc(sizeof(tTableItem))) != NULL)		//kontrola mallocu
+		{
+			//vlozeni typu (FUN) a nazvu (key)
+			new->typ = FUN;
+			strInit(&(new->key));
+			strCopyString(&(new->key), key);
+			new->data->localTable = NULL;		//inicializace lokalni tabulky symbolu
+			T->first = new;			
+		}
+	}
+	else	//zjistime, zda se v tabulce polozka s danym klicem jiz nenachazi a zapiseme ji
+	{
+		while ((!found) && (ptr != NULL) && (end == 0))	//dokud nenajde, nebo nedojde na konec stromu
+		{
+			if(strCmpString(key, &(ptr->key)) == 0)
+			{
+				found = 1;
+				ptr = new = NULL;
+			}
+			else if(strCmpString(key, &(ptr->key)) < 0)	//hledany klic je mensi, prohledej levy podstrom
+				if(ptr->leftptr != NULL) ptr = ptr->leftptr;
+				else end = 1;	//koncime v levem podstromu
+			else if(strCmpString(key, &(ptr->key)) > 0)	//hledany klic je vetsi, prohledej pravy podstrom
+				if(ptr->rightptr != NULL) ptr = ptr->rightptr;
+				else end = 2;	//koncime v pravem podstromu
+		} 
+		if(found==0 && end != 0)	//prvek nebyl nalezen, muzeme proto vlozit novy
+		{
+			if((new = (tTableItem*) malloc(sizeof(tTableItem))) != NULL)		//kontrola mallocu
+			{
+			//vlozeni typu (FUN) a nazvu (key)
+			new->typ = FUN;
+			strInit(&(new->key));
+			strCopyString(&(new->key), key);
+			new->data->localTable = NULL;		//inicializace lokalni tabulky symbolu			
+			if(end == 1) ptr->leftptr = new;		//prvek vlozen do leveho podstromu
+			if(end == 2) ptr->rightptr = new;		//prvek vlozen do praveho podstromu
+			}
+		}
+	}
+	return new;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ial.h
+++ ial.h
@@ -1,6 +1,51 @@
 #include "str.h"
+#include <stdbool.h>
 
-int kmp_search(string S, string W);
-void kmp_table(string W, int *T);
-string merge_sort(string m);
-string merge(string left, string right);
+#define FUN 201
+#define VAR 202
+
+
+int kmp_search(string S, string W);		//Knuth-Moris-Prath algrotimus
+void kmp_table(string W, int *T);		//Knuth-Moris-Prath algrotimus
+
+string merge_sort(string m);			//merge sort
+string merge(string left, string right);//merge sort
+
+void tableInit(tSymbolTable *T)											//inicializace tabulky symbolu
+tTableItem *tableSearch(tSymbolTable *T, string *key)			//hledani polozky v tabulce, vraci odkaz
+tTableItem *tableSearchVar(tSymbolTable *T, string *key)		//hledani promenne v tabulce
+tTableItem *tableSearchFun(tSymbolTable *T, string *key)		//heldani funkce v tabulce
+tTableItem *tableInsertVar(tSymbolTable *T, string *key)		//vlozeni NOVE promenne do tabulky
+tTableItem *tableInsertFun(tSymbolTable *T, string *key)		//vlozeni NOVE funkce do tabulky, inicializuje lokalni tabulku dane funkce
+
+
+union varValue	//unie pro ukladani hodnot promennych v TS
+{	
+	int integer;
+	bool boolean;
+	double real;
+	string str;
+	
+}varValue;
+
+typedef struct
+{
+  int varType;  // typ dane promenne - integer, bool, ...
+  union varValue hodnota; // hodnota promenne pomoci unie
+  /* tList neco	//seznam promennych */
+  tSymbolTable *localTable;	//lokalni tabulka symbolu
+} tData;
+
+typedef struct tableItem
+{
+  int typ;						//funkce, nebo promenna
+  string key;                  // klic, podle ktereho se bude vyhledavat == jmeno promenne/funkce
+  tData data;                  // data, ktera jsou ke klici pridruzena
+  struct tableItem *rightptr;  // ukazatel na pravy podstorm
+  struct tableItem *leftptr;	//ukazatel na levy podstrom
+} tTableItem;
+
+typedef struct
+{
+  struct tableItem *first;
+} tSymbolTable;
